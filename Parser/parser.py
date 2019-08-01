53250878fca16861ca1b24759f8f891a5b8429c8
Ecyn <ecynmare@gmail.com>
2019-08-01 18:22:46 +0700

    Added parser naive impl

--- Parser/parser.py
+++ Parser/parser.py
@@ -3,30 +3,38 @@ import re
 
 
 def parse(url):
-	headers = {
-		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15'
-	}
+	"""
+	Parse given URL and return dictionary of strings (empty string if particular element does not exist).
+	Return None for any urllib error.
+	"""
 	html = ''
-	with request.urlopen(url) as response:
-		html = response.read()
+	try:
+		with request.urlopen(url) as response: # valid check, exceptions catch
+			html = response.read()
+	except:
+		return None
 	encode = re.search("<meta charset=\"([^>]+)\"", str(html))
 	title = re.search("<title>(.*)</title>", str(html))
 	h1 = re.search("<h1>(.*)</h1>", str(html))
-	output = ''
+	output = {}
 
-	if encode is not None:
-		output += encode.group(1)
-	else:
-		output += None
-	if title is not None:
-		output += title.group(1)
-	else:
-		output += None
-	if h1 is not None:
-		output += h1.group(1)
-	else:
-		output += None
+	# if encode is not None:
+	# 	output['encode'] = encode.group(1)
+	# else:
+	# 	output['encode'] = ''
+	# if title is not None:
+	# 	output['title'] = title.group(1)
+	# else:
+	# 	output['title'] = ''
+	# if h1 is not None:
+	# 	output['h1'] = h1.group(1)
+	# else:
+	# 	output['h1'] = ''
+
+	output['h1'] = h1.group(1) if h1 is not None else ''
+	output['title'] = title.group(1) if title is not None else ''
+	output['encode'] = encode.group(1) if encode is not None else ''
 	return output
 
-o = parse('https://docs.python.org/3/howto/urllib2.html')
-print(o)
+# o = parse('https://docs.python.org/3/library/re.html?highlight=re#match-objects')
+# print(o)
--- Parser/views.py
+++ Parser/views.py
@@ -7,8 +7,16 @@ from .parser import parse
 
 def index(request):
 	requests = Request.objects.filter(handling_time__lte = datetime.now(timezone.utc)).order_by('-handling_time')
-	for request in request:
-		console.log(parse(request.url))
+	for request_obj in requests:
+		if ParseResult.objects.filter(request=request_obj).exists():
+			continue
+		page_content = parse(request_obj.url)
+		if page_content is not None:
+			ParseResult.objects.create(
+				request=request_obj,
+				encoding=page_content['encode'],
+				title=page_content['title'],
+				h1=page_content['h1'])
 	results = ParseResult.objects.all()
 	context = {
 		'requests': requests,
