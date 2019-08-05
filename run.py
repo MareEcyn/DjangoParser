281867aeff4e6049f087491fb54587a2f9736a4e
Ecyn <ecynmare@gmail.com>
2019-08-05 10:53:01 +0700

    Added runner script

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import subprocess
 
 if __name__ == '__main__':
 	subprocess.Popen(['python manage.py runserver'], shell=True)
-	subprocess.Popen(['python Parser/server.py'], shell=False)
-	subprocess.Popen(['python', 'Parser/scheduler.py'], shell=False)
+	subprocess.Popen(['python', 'Parser/server.py'], shell=False)
+	subprocess.Popen(['python', 'manage.py', 'runscheduler'], shell=False)
